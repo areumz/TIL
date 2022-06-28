8f813edec116e556478accc4696ba44e4b5d600b
Areum Kim <84116709+areumz@users.noreply.github.com>
2022-06-28 23:37:21 +0900

    Create App.js

---
+++ TDD/jest-test/color-button/src/App.js
@@ -0,0 +1,20 @@
+// 배포용 App 파일
+
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+    const [buttonColor, setColor] = useState('red');
+    const newColor = buttonColor === 'red' ? 'blue' : 'red';
+
+    return (
+        <div>
+            <button style={{backgroundColor: buttonColor}} onClick={()=>{
+                setColor(newColor)
+            }}>change to {newColor}!</button>
+        </div>
+    );
+}
+
+export default App;
